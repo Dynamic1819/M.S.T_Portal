ff04cc8e8a8b2036cb66c4af5261bad76d697589
Dynamic1819 <129028703+Dynamic1819@users.noreply.github.com>
2023-08-14 08:46:32 +0630

    First commit

---
+++ tailwind.config.js
@@ -0,0 +1,24 @@
+/** @type {import('tailwindcss').Config} */
+const defaultTheme = require('tailwindcss/defaultTheme')
+module.exports = {
+  content: ["public/*.html", "./node_modules/flowbite/**/*.js"],
+  theme: {
+    screens: {
+      'mdl':'896px',
+      'xs': '415px',
+      'xms': '370px',
+      'xxs': '300px',
+      ...defaultTheme.screens,
+    },
+    extend: {
+      colors:{
+        primary: '#041562',
+        secondary:'#9a0004',
+        color1:'#484b6a',
+        color2:'#9394a5',
+      },
+    },
+  },
+  plugins: [require("daisyui")],
+}
+
